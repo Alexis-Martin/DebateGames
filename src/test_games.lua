9abd0c5ef8252862529bcc622335b3c13f1aa6ef
Alexis-Martin <martinalexis90@gmail.com>
2016-10-13 11:04:30 +0200

    changes

--- src/test_games.lua
+++ src/test_games.lua
@@ -90,9 +90,9 @@ local function test_random_games(val_question)
   end
 end
 -- --
-do
-  test_random_games(1)
-end
+-- do
+--   test_random_games(1)
+-- end
 
 
 
@@ -272,170 +272,180 @@ end
 
 
 -- STATS MOYENNE AND AGGREGATION
--- do
---   local function stats(nb_tests, nb_players, nb_vertices, graph_fun, param)
---     local values_mean  = {}
---     local values_agg   = {}
---     local lms          = {}
---     local lms_mean     = {}
---     local lms_agg      = {}
---     local nb_same_mean = 0
---     local nb_same_agg  = 0
---     local total_test   = nb_tests
---
---     while nb_tests > 0 do
---       nb_tests       = nb_tests - 1
---       if nb_tests % 100 == 0 then
---         print("intern test : " .. nb_tests)
---       end
---       local graph    = graph_fun(nb_vertices)
---       local game     = game_generator(nb_players, graph)
---       local rule     = mind_changed.create(game)
---       if param then rule:setParameters(param) end
---       rule:apply()
---
---       -- save results
---       local dist_LM_m = math.abs(game:getLM()[#game:getLM()].value - game:getTag("mean"))
---
---       if dist_LM_m == 0 then nb_same_mean = nb_same_mean + 1 end
---
---       if not values_mean.min then
---         values_mean.min = tools.round(dist_LM_m, 2)
---       elseif values_mean.min > dist_LM_m then
---         values_mean.min = tools.round(dist_LM_m, 2)
---       end
---
---       if not values_mean.max then
---         values_mean.max = tools.round(dist_LM_m, 2)
---       elseif values_mean.max < dist_LM_m then
---         values_mean.max = tools.round(dist_LM_m, 2)
---       end
---       table.insert(lms_mean, dist_LM_m)
---
---       local dist_LM_a = math.abs(game:getLM()[#game:getLM()].value - game:getTag("aggregate_value"))
---
---       if dist_LM_a == 0 then nb_same_agg = nb_same_agg + 1 end
---
---       if not values_agg.min then
---         values_agg.min = tools.round(dist_LM_a, 2)
---       elseif values_agg.min > dist_LM_a then
---         values_agg.min = tools.round(dist_LM_a, 2)
---       end
---
---       if not values_agg.max then
---         values_agg.max = tools.round(dist_LM_a, 2)
---       elseif values_agg.max < dist_LM_a then
---         values_agg.max = tools.round(dist_LM_a, 2)
---       end
---       table.insert(lms, game:getLM()[#game:getLM()].value)
---       table.insert(lms_agg     , dist_LM_a)
---     end
---
---     values_mean.moyenne_valeur_jeu = 0
---     for _, v in ipairs(lms) do
---       values_mean.moyenne_valeur_jeu = values_mean.moyenne_valeur_jeu + v
---     end
---     values_mean.moyenne_valeur_jeu = tools.round(values_mean.moyenne_valeur_jeu / #lms, 2)
---
---     values_mean.mean = 0
---     for _, v in ipairs(lms_mean) do
---       values_mean.mean = values_mean.mean + v
---     end
---     values_mean.mean = tools.round(values_mean.mean / #lms_mean, 2)
---
---     table.sort(lms_mean)
---     if #lms_mean % 4 == 0 then
---       values_mean.q1   = tools.round(lms_mean[#lms_mean/4], 2)
---       values_mean.q3   = tools.round(lms_mean[(#lms_mean/4) * 3], 2)
---       values_mean.med  = tools.round(lms_mean[#lms_mean/2], 2)
---     else
---       local f = math.floor(#lms_mean/4)
---       values_mean.q1   = tools.round((lms_mean[f] + lms_mean[f+1]) / 2, 2)
---       values_mean.q3   = tools.round((lms_mean[f * 3] + lms_mean[f * 3+1]) / 2, 2)
---       values_mean.med  = tools.round((lms_mean[math.floor(#lms_mean/2)] + lms_mean[math.ceil(#lms_mean/2)]) / 2, 2)
---     end
---
---     values_agg.moyenne_valeur_jeu = values_mean.moyenne_valeur_jeu
---     values_agg.mean = 0
---     for _, v in ipairs(lms_agg) do
---       values_agg.mean = values_agg.mean + v
---     end
---     values_agg.mean = tools.round(values_agg.mean / #lms_agg, 2)
---
---     table.sort(lms_agg)
---     if #lms_agg % 4 == 0 then
---       values_agg.q1   = tools.round(lms_agg[#lms_agg/4], 2)
---       values_agg.q3   = tools.round(lms_agg[(#lms_agg/4) * 3], 2)
---       values_agg.med  = tools.round(lms_agg[#lms_agg/2], 2)
---     else
---       local f = math.floor(#lms_agg/4)
---       values_agg.q1   = tools.round((lms_agg[f] + lms_agg[f]) / 2, 2)
---       values_agg.q3   = tools.round((lms_agg[f * 3] + lms_agg[f * 3 + 1]) / 2, 2)
---       values_agg.med  = tools.round((lms_agg[math.floor(#lms_agg/2)] + lms_agg[math.ceil(#lms_agg/2)]) / 2, 2)
---     end
---
---     nb_same_mean = (nb_same_mean / total_test) * 100
---     nb_same_agg  = (nb_same_agg  / total_test) * 100
---
---     return values_mean, values_agg, nb_same_mean, nb_same_agg
---   end
---
---
---   local max_players  = 8
---   local nb_vertices  = 15
---   local graphGen     = graph_generator.generateTree
---   local nb_tests     = 5000
---   local x_label      = "nombres de joueurs"
---   local y_label      = "valeur du jeu"
---   local title_mean   = "Distance à la moyenne, Arbre, " .. nb_vertices .. " arguments (" .. nb_tests .. " tests)"
---   local title_agg    = "Distance à l'aggregation, Arbre, " .. nb_vertices .. " arguments (" .. nb_tests .. " tests)"
---   local output_mean  = "../tests/stats/" .. nb_vertices .. "_" .. nb_tests .. "_mean.tex"
---   local output_agg  = "../tests/stats/" .. nb_vertices .. "_" .. nb_tests .. "_agg.tex"
---   local color_mean   = {
---     moyenne_valeur_jeu = "0066ff",
---     mean         = "cc0000"
---   }
---   local color_agg    = {
---     moyenne_valeur_jeu  = "0066ff",
---     mean                = "cc0000"
---   }
---
---
---   -- rules parameters
---   local param = {
---     compute_agg  = true,
---     compute_mean = true,
---     start        = "aggregation"
---   }
---
---   local save_values_m = {}
---   local save_values_a = {}
---   local success_m     = {}
---   local success_a     = {}
---
---   for i = 2, max_players do
---     local values_m, values_a, percent_m, percent_a = stats(nb_tests, i, nb_vertices, graphGen, param)
---     values_m.x = i
---     values_a.x = i
---     table.insert(save_values_a, values_a)
---     table.insert(save_values_m, values_m)
---     success_a[i] = percent_a
---     success_m[i] = percent_m
---   end
---   save_values_m.color   = color_mean
---   save_values_m.x_label = x_label
---   save_values_m.y_label = y_label
---   save_values_m.title   = title_mean
---   save_values_m.success = success_m
---   boxplot(save_values_m, output_mean)
---
---   save_values_a.color   = color_agg
---   save_values_a.x_label = x_label
---   save_values_a.y_label = y_label
---   save_values_a.title   = title_agg
---   save_values_a.success = success_a
---   boxplot(save_values_a, output_agg)
--- end
+do
+  local function stats(nb_tests, nb_players, nb_vertices, graph_fun, param)
+    local values_mean  = {}
+    local values_agg   = {}
+    local lms          = {}
+    local lms_mean     = {}
+    local lms_agg      = {}
+    local nb_same_mean = 0
+    local nb_same_agg  = 0
+    local total_test   = nb_tests
+    local insane_dist  = false
+
+    while nb_tests > 0 do
+      nb_tests       = nb_tests - 1
+      if nb_tests % 100 == 0 then
+        print("intern test : " .. nb_tests)
+        io.flush()
+      end
+      local graph    = graph_fun(nb_vertices)
+      local game     = game_generator(nb_players, graph)
+      local rule     = mind_changed.create(game)
+      if param then rule:setParameters(param) end
+      rule:apply()
+
+      -- save results
+      local dist_LM_m = math.abs(game:getLM()[#game:getLM()].value - game:getTag("mean"))
+
+      if dist_LM_m == 0 then nb_same_mean = nb_same_mean + 1 end
+
+      if not values_mean.min then
+        values_mean.min = tools.round(dist_LM_m, 2)
+      elseif values_mean.min > dist_LM_m then
+        values_mean.min = tools.round(dist_LM_m, 2)
+      end
+
+      if not values_mean.max then
+        values_mean.max = tools.round(dist_LM_m, 2)
+      elseif values_mean.max < dist_LM_m then
+        values_mean.max = tools.round(dist_LM_m, 2)
+      end
+      table.insert(lms_mean, dist_LM_m)
+
+      local dist_LM_a = math.abs(game:getLM()[#game:getLM()].value - game:getTag("aggregate_value"))
+
+      if dist_LM_a == 0 then nb_same_agg = nb_same_agg + 1 end
+
+      if not values_agg.min then
+        values_agg.min = tools.round(dist_LM_a, 2)
+      elseif values_agg.min > dist_LM_a then
+        values_agg.min = tools.round(dist_LM_a, 2)
+      end
+
+      if not values_agg.max then
+        values_agg.max = tools.round(dist_LM_a, 2)
+      elseif values_agg.max < dist_LM_a then
+        values_agg.max = tools.round(dist_LM_a, 2)
+      end
+
+      if values_agg.max > 0.70 and not insane_dist then
+        game:exportXml("../tests/stats/insane_game_" .. nb_players .. "_" .. nb_vertices .. ".xml", "all")
+        game:exportTex("../tests/stats/insane_game_" .. nb_players .. "_" .. nb_vertices .. ".tex")
+        insane_dist = true
+      end
+
+      table.insert(lms, game:getLM()[#game:getLM()].value)
+      table.insert(lms_agg     , dist_LM_a)
+    end
+
+    values_mean.moyenne_valeur_jeu = 0
+    for _, v in ipairs(lms) do
+      values_mean.moyenne_valeur_jeu = values_mean.moyenne_valeur_jeu + v
+    end
+    values_mean.moyenne_valeur_jeu = tools.round(values_mean.moyenne_valeur_jeu / #lms, 2)
+
+    values_mean.mean = 0
+    for _, v in ipairs(lms_mean) do
+      values_mean.mean = values_mean.mean + v
+    end
+    values_mean.mean = tools.round(values_mean.mean / #lms_mean, 2)
+
+    table.sort(lms_mean)
+    if #lms_mean % 4 == 0 then
+      values_mean.q1   = tools.round(lms_mean[#lms_mean/4], 2)
+      values_mean.q3   = tools.round(lms_mean[(#lms_mean/4) * 3], 2)
+      values_mean.med  = tools.round(lms_mean[#lms_mean/2], 2)
+    else
+      local f = math.floor(#lms_mean/4)
+      values_mean.q1   = tools.round((lms_mean[f] + lms_mean[f+1]) / 2, 2)
+      values_mean.q3   = tools.round((lms_mean[f * 3] + lms_mean[f * 3+1]) / 2, 2)
+      values_mean.med  = tools.round((lms_mean[math.floor(#lms_mean/2)] + lms_mean[math.ceil(#lms_mean/2)]) / 2, 2)
+    end
+
+    values_agg.moyenne_valeur_jeu = values_mean.moyenne_valeur_jeu
+    values_agg.mean = 0
+    for _, v in ipairs(lms_agg) do
+      values_agg.mean = values_agg.mean + v
+    end
+    values_agg.mean = tools.round(values_agg.mean / #lms_agg, 2)
+
+    table.sort(lms_agg)
+    if #lms_agg % 4 == 0 then
+      values_agg.q1   = tools.round(lms_agg[#lms_agg/4], 2)
+      values_agg.q3   = tools.round(lms_agg[(#lms_agg/4) * 3], 2)
+      values_agg.med  = tools.round(lms_agg[#lms_agg/2], 2)
+    else
+      local f = math.floor(#lms_agg/4)
+      values_agg.q1   = tools.round((lms_agg[f] + lms_agg[f]) / 2, 2)
+      values_agg.q3   = tools.round((lms_agg[f * 3] + lms_agg[f * 3 + 1]) / 2, 2)
+      values_agg.med  = tools.round((lms_agg[math.floor(#lms_agg/2)] + lms_agg[math.ceil(#lms_agg/2)]) / 2, 2)
+    end
+
+    nb_same_mean = (nb_same_mean / total_test) * 100
+    nb_same_agg  = (nb_same_agg  / total_test) * 100
+
+    return values_mean, values_agg, nb_same_mean, nb_same_agg
+  end
+
+
+  local max_players  = 8
+  local nb_vertices  = 10
+  local graphGen     = graph_generator.generateTree
+  local nb_tests     = 5000
+  local x_label      = "nombres de joueurs"
+  local y_label      = "valeur du jeu"
+  local title_mean   = "Distance à la moyenne, Arbre, " .. nb_vertices .. " arguments (" .. nb_tests .. " tests)"
+  local title_agg    = "Distance à l'aggregation, Arbre, " .. nb_vertices .. " arguments (" .. nb_tests .. " tests)"
+  local output_mean  = "../tests/stats/" .. nb_vertices .. "_" .. nb_tests .. "_mean.tex"
+  local output_agg  = "../tests/stats/" .. nb_vertices .. "_" .. nb_tests .. "_agg.tex"
+  local color_mean   = {
+    moyenne_valeur_jeu = "0066ff",
+    mean         = "cc0000"
+  }
+  local color_agg    = {
+    moyenne_valeur_jeu  = "0066ff",
+    mean                = "cc0000"
+  }
+
+
+  -- rules parameters
+  local param = {
+    compute_agg  = true,
+    compute_mean = true,
+    start        = "aggregation"
+  }
+
+  local save_values_m = {}
+  local save_values_a = {}
+  local success_m     = {}
+  local success_a     = {}
+
+  for i = 2, max_players do
+    local values_m, values_a, percent_m, percent_a = stats(nb_tests, i, nb_vertices, graphGen, param)
+    values_m.x = i
+    values_a.x = i
+    table.insert(save_values_a, values_a)
+    table.insert(save_values_m, values_m)
+    success_a[i] = percent_a
+    success_m[i] = percent_m
+  end
+  save_values_m.color   = color_mean
+  save_values_m.x_label = x_label
+  save_values_m.y_label = y_label
+  save_values_m.title   = title_mean
+  save_values_m.success = success_m
+  boxplot(save_values_m, output_mean)
+
+  save_values_a.color   = color_agg
+  save_values_a.x_label = x_label
+  save_values_a.y_label = y_label
+  save_values_a.title   = title_agg
+  save_values_a.success = success_a
+  boxplot(save_values_a, output_agg)
+  io.flush()
+end
 
 
 
